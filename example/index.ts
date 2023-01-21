7f7c50c33e4c3c6465a13d38ce82d144e671f204
4lessandrodev <alessandroadm@live.com>
2023-01-21 04:28:12 -0300

    docs(example): update example using utils

--- example/index.ts
+++ example/index.ts
@@ -12,6 +12,16 @@ export class MyValueObject extends ValueObject<Props>{
 		return false;
 	}
 
+	sum(value: number): number {
+		const current  = this.props.value;
+		return this.util.number(current).sum(value);
+	}
+
+	isEven(): boolean {
+		const current  = this.props.value;
+		return this.validator.number(current).isEven();
+	}
+
 	public static create(props: Props): Result<MyValueObject> {
 		return Result.Ok(new MyValueObject(props));
 	}
@@ -21,5 +31,9 @@ const result = MyValueObject.create({ value: 42 });
 
 assert.equal(result.isOk(), true, 'the result is failure');
 assert.equal(result.value().get('value'), 42, 'the value is not 42');
+assert.equal(result.value().sum(1), 43, 'the value is not 43');
+assert.equal(result.value().isEven(), true, 'the value is even');
 
-result.value().set('value').to(20);
+// validation is set to false
+const changed = result.value().set('value').to(20);
+assert.equal(changed, false, 'value changed');
