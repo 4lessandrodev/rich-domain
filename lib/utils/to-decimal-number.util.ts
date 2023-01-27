209ebe8706c08ee714ad569f0e6a8b3ba609a84d
4lessandrodev <alessandroadm@live.com>
2023-01-27 11:50:50 -0300

    refactor(utils): change base value from 10 to 100

--- lib/utils/to-decimal-number.util.ts
+++ lib/utils/to-decimal-number.util.ts
@@ -1,6 +1,6 @@
 export const ToDecimal = (value: number): number => {
     const isValid = typeof value === 'number';
     if(!isValid) return value;
-    return value / 10;
+    return value / 100;
 }
 export default ToDecimal;
--- lib/utils/to-long-number.util.ts
+++ lib/utils/to-long-number.util.ts
@@ -1,6 +1,6 @@
 export const ToLong = (value: number): number => {
     const isValid = typeof value === 'number';
     if(!isValid) return value;
-    return value * 10;
+    return value * 100;
 }
 export default ToLong;
