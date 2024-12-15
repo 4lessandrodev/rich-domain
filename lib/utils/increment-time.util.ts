3009ad4f8a3dd27af83c6d3df6ac276c6a4d21db
4lessandrodev <alessandroadm@live.com>
2024-12-15 18:02:55 -0300

    docs: update tsdocs comments at code

--- lib/utils/increment-time.util.ts
+++ lib/utils/increment-time.util.ts
@@ -1,14 +1,36 @@
 import { ONE_DAY, ONE_HOUR, ONE_MINUTE, ONE_MONTH, ONE_WEEK, ONE_YEAR, Unit } from "../types";
 
+/**
+ * @description Increments a given date by a specified amount of time based on the unit provided.
+ * 
+ * @param date The starting date to increment.
+ * @param value The number of units to increment. If not a valid number, the original timestamp is returned.
+ * @param unit The unit of time to increment. Possible values are:
+ * - `'day'`: Increment by days.
+ * - `'hour'`: Increment by hours.
+ * - `'minute'`: Increment by minutes.
+ * - `'month'`: Increment by months.
+ * - `'week'`: Increment by weeks.
+ * - `'year'`: Increment by years.
+ * 
+ * @returns The incremented timestamp as a number. If the input date is invalid, the current timestamp is returned.
+ * 
+ * @example
+ * ```typescript
+ * const now = new Date();
+ * const incrementedTime = IncrementTime(now, 3, 'day'); // Adds 3 days to the current date.
+ * console.log(new Date(incrementedTime)); // Logs the incremented date.
+ * ```
+ */
 export const IncrementTime = (date: Date, value: number, unit: Unit): number => {
 
-    if(!(date instanceof Date)) return new Date().getTime();
+    if (!(date instanceof Date)) return new Date().getTime();
 
     const time = date.getTime();
 
-    if(typeof value !== 'number') return time;
+    if (typeof value !== 'number') return time;
 
-    switch(unit){
+    switch (unit) {
         case 'day': return (ONE_DAY * value) + time;
         case 'hour': return (ONE_HOUR * value) + time;
         case 'minute': return (ONE_MINUTE * value) + time;
