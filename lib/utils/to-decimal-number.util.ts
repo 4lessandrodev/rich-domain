1fbef3b099f9bc9cc5182ca1495814d0f715905d
4lessandrodev <alessandroadm@live.com>
2024-12-15 17:58:11 -0300

    docs: update tsdocs comments at code

--- lib/utils/to-decimal-number.util.ts
+++ lib/utils/to-decimal-number.util.ts
@@ -1,6 +1,23 @@
+/**
+ * @description Converts a number to its decimal equivalent by dividing it by 100.
+ * This is useful for normalizing values, such as converting percentages to decimals.
+ * 
+ * @param value The input value to convert. Must be a number.
+ * 
+ * @returns The decimal equivalent of the input number. 
+ * If the input is not a valid number, it returns the input as is.
+ * 
+ * @example
+ * ```typescript
+ * ToDecimal(500); // Returns 5
+ * ToDecimal(25); // Returns 0.25
+ * ToDecimal("100"); // Returns "100" (input not a valid number)
+ * ```
+ */
 export const ToDecimal = (value: number): number => {
     const isValid = typeof value === 'number';
-    if(!isValid) return value;
+    if (!isValid) return value;
     return value / 100;
 }
+
 export default ToDecimal;
