37cce5de8b5d3da974a64652aedcfc4888ec6b68
4lessandrodev <alessandroadm@live.com>
2024-12-15 18:02:40 -0300

    docs: update tsdocs comments at code

--- lib/utils/is-nan.util.ts
+++ lib/utils/is-nan.util.ts
@@ -1,4 +1,20 @@
-export const IsNaN = (value: string | number) : boolean => {
+/**
+ * @description Checks if the provided value is `NaN` (Not-a-Number). The function attempts to parse the value into a number
+ * and determines if the result is `NaN`.
+ * 
+ * @param value The value to check. Can be a string or a number.
+ * 
+ * @returns `true` if the value is `NaN`, otherwise `false`.
+ * 
+ * @example
+ * ```typescript
+ * IsNaN("123"); // Returns false (valid number)
+ * IsNaN("abc"); // Returns true (not a number)
+ * IsNaN(NaN); // Returns true
+ * IsNaN(123); // Returns false (valid number)
+ * ```
+ */
+export const IsNaN = (value: string | number): boolean => {
     return isNaN(parseFloat(String(value)));
 }
 
