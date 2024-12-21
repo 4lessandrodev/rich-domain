a01d616ad5d29b5bb66dba925171516b2cdd3f45
4lessandrodev <alessandroadm@live.com>
2024-12-20 21:30:05 -0300

    fix: ensure tests for revert #194

--- tests/core/adapter.spec.ts
+++ tests/core/adapter.spec.ts
@@ -104,7 +104,7 @@ describe('adapter v1', () => {
 		type Err = { err: string; stack?: string };
 
 		class CustomAdapter implements _Adapter<In, Out, Err> {
-			build(target: In): _Result<Out | null, Err> {
+			build(target: In): _Result<Out, Err> {
 				if (typeof target.a !== 'number') return Fail({ err: 'target.a is not a number' });
 				return Ok({ b: target.a.toString() });
 			}
